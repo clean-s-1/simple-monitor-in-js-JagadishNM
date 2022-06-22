e04d2a29c0d63d2336cc4235989a0d41cd0af968
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-06-22 11:51:05 +0530

    Add files via upload

--- showMessage.js
+++ showMessage.js
@@ -1,22 +1,32 @@
-import { checkNumberRange } from './check.range.js'
-export function showMessage(range, soc, type) {
+import { checkNumberRange } from './check.range.js';
+
+export function showMessage(range, currentValue, type) {
     for (const obj in range) {
-        const isRange = checkNumberRange(range[obj][0], range[obj][1], soc);
-        if (obj == 'LOW_BREACH' && isRange) {
-            console.log(`${type} is LOW_BREACH`);
-            return false;
-        } else if (obj == 'LOW__WARNING' && isRange) {
-            console.log(`${type} is LOW__WARNING`);
-            return false;
-        } else if (obj == 'NORMAL' && isRange) {
-            console.log(`${type} is NORMAL`);
-            return false;
-        } else if (obj == 'HIGH_WARNING' && isRange) {
-            console.log(`${type} is HIGH_WARNING`);
-            return false;
-        } else if (obj == 'HIGH_BREACH' && isRange) {
-            console.log(`${type} is HIGH_BREACH`);
-            return false;
+        const isRange = checkNumberRange(range[obj][0], range[obj][1], currentValue);
+        if(isRange){
+            let messageType = findMessage(obj, type);
+            messageType();
         }
     }
+}
+
+function findMessage(messageName, type){
+    return {
+        LOW_BREACH: function () {
+            console.log(`${type} is LOW_BREACH`);
+        },
+        LOW__WARNING: function () {
+            console.log(`${type} is LOW__WARNING`);
+        },
+        NORMAL: function () {
+            console.log(`${type} is NORMAL`);
+        },
+        HIGH_WARNING: function () {
+            console.log(`${type} is HIGH_WARNING`);
+        },
+        HIGH_BREACH: function () {
+            console.log(`${type} is HIGH_BREACH`);
+        }
+    }[messageName]
+    
 }
